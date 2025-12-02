5b5095e282371dd8782d8707d3b6f46d2b6fba1b
alex-fdr <alexfv01@gmail.com>
2025-12-02 12:12:44 +0200

    remove unused utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export { applyTransform, type TransformProps } from './apply-transform';
+export { deepMerge } from './deep-merge';
 export { getObjectSize } from './get-object-size';
 export { getScreenSize } from './get-screen-size';
-export { throttleTrailing } from './throttle';
-export { deepMerge } from './deep-merge';
