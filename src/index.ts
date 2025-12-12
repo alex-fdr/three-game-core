b9a540db32b238d5cf45adef737b081c6286227e
alex-fdr <alexfv01@gmail.com>
2025-12-12 22:24:24 +0200

    re-export Signal class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { GameCore } from './core';
 
 export { type AssetsData, assets } from './assets';
+export { Signal } from './helpers/signal';
 export type { InputHandler, InputStatus } from './input';
 export type { GameSettings } from './settings';
 export * as utils from './utils';
